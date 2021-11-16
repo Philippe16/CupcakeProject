b542aedf9df95cd89ea72ac6daaa41a9deda2a3d
Angeliza Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2021-11-16 19:22:18 +0100

    Refactor code

--- Web/sem2-startcode-main/src/main/java/web/commands/BuyCupcakesCommand.java
+++ Web/sem2-startcode-main/src/main/java/web/commands/BuyCupcakesCommand.java
@@ -5,54 +5,70 @@ import business.entities.ShoppingBasket;
 import business.entities.User;
 import business.exceptions.UserException;
 import business.services.OrderFacade;
+import business.services.UserFacade;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 
 public class BuyCupcakesCommand extends CommandUnprotectedPage {
    OrderFacade orderFacade;
+   UserFacade userFacade;
 
    public BuyCupcakesCommand(String pageToShow) {
       super(pageToShow);
       orderFacade = new OrderFacade(database);
+      userFacade = new UserFacade(database);
    }
 
    @Override
    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
       try {
          HttpSession session = request.getSession();
-
-         if(ShoppingBasket.getOrderItems().size() == 0){
-            throw new UserException("Your basket is still empty!");
-         }
-
          User user = (User) session.getAttribute("user");
+         Double orderPrice = (Double) session.getAttribute("shoppingBasketTotalPrice");
 
-         if(user == null){
-            throw new UserException("You need to be signed in to buy cupcakes");
-         }
+         /* Test if purchase is doable */
+         checkIfBasketIsEmpty();
+         checkIfUserIsSignedIn(user);
+         checkIfUserBalanceIsEnough(user, orderPrice);
 
-         Double price = (Double) session.getAttribute("shoppingBasketTotalPrice");
-
-         if(user.getAccountBalance() < price){
-            throw new UserException("Your account balance is too low...");
-         }
-
-         /* Send order items to the database */
+         /* Send order to the database */
          orderFacade.createOrder(user, ShoppingBasket.getOrderItems());
 
-         /* Empty the ShoppingBasket's orderItems list + Update session attributes */
+         /* Update user account balance */
+
+         /* Empty the ShoppingBasket's orderItems list */
          ShoppingBasket.getOrderItems().clear();
+
+         /* Update session attributes */
          session.setAttribute("shoppingBasketItems", ShoppingBasket.getOrderItems());
          session.setAttribute("shoppingBasketTotalPrice", ShoppingBasket.getTotalPrice());
          session.setAttribute("shoppingBasketTotalCupcakes", ShoppingBasket.getTotalCupcakes());
+         session.setAttribute("orderConfirmation", true);
 
          return REDIRECT_INDICATOR + "shoppingBasket";
       } catch (UserException ex) {
          request.setAttribute("error", ex.getMessage());
          return "shoppingBasket";
+      }
+   }
+
+   private void checkIfBasketIsEmpty() throws UserException{
+      if(ShoppingBasket.getOrderItems().size() == 0){
+         throw new UserException("Your basket is still empty!");
+      }
+   }
+
+   private void checkIfUserIsSignedIn(User user) throws UserException{
+      if(user == null){
+         throw new UserException("You need to be signed in to buy cupcakes");
+      }
+   }
+
+   private void checkIfUserBalanceIsEnough(User user, Double orderPrice) throws UserException{
+      if(user.getAccountBalance() < orderPrice){
+         throw new UserException("Your account balance is too low...");
       }
    }
 
